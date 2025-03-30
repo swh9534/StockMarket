dd2794c12266b7f91743e5170315f2420a26660b
Haemin Kim <haemin1230@gmail.com>
2025-03-30 23:06:18 +0900

    frontendController 추가

--- backend/src/main/java/com/example/stockmarket/controller/FrontendController.java
+++ backend/src/main/java/com/example/stockmarket/controller/FrontendController.java
@@ -1,4 +1,15 @@
 package com.example.stockmarket.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class FrontendController {
+
+    @RequestMapping(value = {
+            "/", "/stock", "/student/**", "/teacher/**"
+    })
+    public String forward() {
+        return "forward:/index.html";
+    }
 }
